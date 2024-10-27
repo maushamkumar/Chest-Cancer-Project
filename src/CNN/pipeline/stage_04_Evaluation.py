4410c6955bef86c4c21282adb483ee51f2475e65
maushamkumar <maushamroy143@gmail.com>
2024-10-27 20:32:21 +0530

    Dvc initilize

--- src/CNN/pipeline/stage_04_Evaluation.py
+++ src/CNN/pipeline/stage_04_Evaluation.py
@@ -14,7 +14,7 @@ class EvaluateModel:
         eval_config = config.get_evaluation_config()
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()         # Runs the model evaluation
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
         
 if __name__ == "__main__":
     try:
